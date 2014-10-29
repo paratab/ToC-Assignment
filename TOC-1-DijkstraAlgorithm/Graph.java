148409a2348aa2d1e24ead46319c8a21cfbe723a
paratab <paratab@users.noreply.github.com>
2014-10-29 15:13:27 +0700

    Add comment & Reposition

--- TOC-1-DijkstraAlgorithm/Graph.java
+++ TOC-1-DijkstraAlgorithm/Graph.java
@@ -1,24 +1,27 @@
 import java.util.*;
 
+//This class is represent the Graph that contains verties and edges
 public class Graph {
 	
 	public ArrayList<Vertex> vertex = new ArrayList<Vertex>();
 	public ArrayList<Edge> edge = new ArrayList<Edge>();
+	
+	//Find the vertex in the graph.
 	public Vertex find(String name){
 		for(Vertex u : vertex){
 			if(u.name.equals(name)) {
-				//System.out.println(name+","+u.name);
 				return u;
 			}
 		}
 		return null;
 	}
 
-	public double getWeight(Vertex u,Vertex v){
+	//Get the weight of the edge 
+	public double getWeight(Vertex start ,Vertex dest){
 		for( Edge e : edge){
-			if(e.start==u&&e.dest==v) return e.weight;
+			if(e.start == start && e.dest == dest) return e.weight;
 		}
-		return Double.POSITIVE_INFINITY;
+		// Return infinity if not found that edge.
+		return Double.POSITIVE_INFINITY;	
 	}
-	
 }
