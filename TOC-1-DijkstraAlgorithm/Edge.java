625f8f7336c6ed95befad8d0196bc4c50bbe6147
paratab <paratab@users.noreply.github.com>
2014-10-29 15:07:55 +0700

    Add Comment & Reposition

--- TOC-1-DijkstraAlgorithm/Edge.java
+++ TOC-1-DijkstraAlgorithm/Edge.java
@@ -1,14 +1,14 @@
-
+//This class is represent the Edge of the graph
 public class Edge {
 	public Vertex start;
 	public Vertex dest;
 	public double weight;
+	//Default Constructor
+	public Edge() {
+	}
 	public Edge(Vertex start,Vertex dest,int weight){
 		this.start = start;
 		this.dest = dest;
 		this.weight = weight;
-	}
-	public Edge() {
-		
 	}
 }
